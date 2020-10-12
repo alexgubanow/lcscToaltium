1e26dff8d17461a2e794ecd60bc65bc4b0aa875f
Oleksandr Hubanov <alex@0x0h.com>
2020-10-12 15:07:18 +0300

    initial dropinn

---
+++ consoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace consoleApp
+{
+    public class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var responseString = await client.GetStringAsync("https://easyeda.com/api/components/8dc274d742c945d894162854d2ea79f5");
+            var response = JsonSerializer.Deserialize<lcsclib.GenericResponse>(responseString);
+            if (response.success == true)
+            {
+                var component = JsonSerializer.Deserialize<lcsclib.FootprintResponse>(response.result.GetRawText());
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*public static T ToObject<T>(this JsonElement element)
+{
+    var json = element.GetRawText();
+    return JsonSerializer.Deserialize<T>(json);
+}
+public static T ToObject<T>(this JsonDocument document)
+{
+    var json = document.RootElement.GetRawText();
+    return JsonSerializer.Deserialize<T>(json);
+}*/
---
+++ lcsclib/LCSCstructs.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+
+namespace lcsclib
+{
+    public struct GenericResponse
+    {
+        public bool success { get; set; }
+        public int code { get; set; }
+        public JsonElement result { get; set; }
+    }
+    public struct Owner
+    {
+        public string uuid { get; set; }
+        public string username { get; set; }
+        public string nickname { get; set; }
+        public string avatar { get; set; }
+    }
+    public struct ComponentResponse
+    {
+        public Owner owner { get; set; }
+        public string uuid { get; set; }
+        public string description { get; set; }
+        public int docType { get; set; }
+        public JsonElement dataStr { get; set; }
+        public string title { get; set; }
+        public string[] tags { get; set; }
+        public int type { get; set; }
+        public int updateTime { get; set; }
+        public string updated_at { get; set; }
+        public bool writable { get; set; }
+        public bool isFavorite { get; set; }
+        public JsonElement packageDetail { get; set; }
+        public bool verify { get; set; }
+    }
+    public struct FootprintResponse
+    {
+        public Owner owner { get; set; }
+        public string uuid { get; set; }
+        public string description { get; set; }
+        public int docType { get; set; }
+        public Footprint dataStr { get; set; }
+        public string title { get; set; }
+        public string[] tags { get; set; }
+        public int type { get; set; }
+        public int updateTime { get; set; }
+        public string updated_at { get; set; }
+        public bool verify { get; set; }
+        public bool writable { get; set; }
+        public bool isFavorite { get; set; }
+    }
+    public struct Footprint
+    {
+        public JsonElement head { get; set; }
+        public string canvas { get; set; }
+        public string[] shape { get; set; }
+        public string systemColor { get; set; }
+        public string[] layers { get; set; }
+        public BBox BBox { get; set; }
+    }
+    public struct BBox
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
+    }
+}

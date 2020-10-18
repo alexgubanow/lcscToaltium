4b841142fed88ecb744f764c579869d2d1556415
Oleksandr Hubanov <alex@0x0h.com>
2020-10-19 01:56:22 +0300

    added few more test requests

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -14,7 +14,13 @@ namespace consoleApp
             await RequestAndSave("https://easyeda.com/api/components/8dc274d742c945d894162854d2ea79f5");
             await RequestAndSave("https://easyeda.com/api/components/0be27f9ebfef4a48b228771721089682");
             await RequestAndSave("https://easyeda.com/api/components/3ae51f9fa3264b7ab826ce43677ca53e");
-            await RequestAndSave("https://easyeda.com/api/components/437735bee9444a5d9dd435521a21a6af");
+            await RequestAndSave("https://easyeda.com/api/components/437735bee9444a5d9dd435521a21a6af");//micro-usb
+            await RequestAndSave("https://easyeda.com/api/components/da48df861c744d6795fcb3db9fd9f7b4");//CAP-SMD_BD10.0-L10
+            await RequestAndSave("https://easyeda.com/api/components/3be10a76f83f422aa182e110e9d3dfed");//sop-5
+            await RequestAndSave("https://easyeda.com/api/components/095c98d119cf4f3089d4e82d07b27c72");//WJ301V-5.0-2P
+            await RequestAndSave("https://easyeda.com/api/components/2ada8f4ae55543e0922c8ee62e571dd5");//led0603
+            await RequestAndSave("https://easyeda.com/api/components/bbc61cb2bd3840b290cf4f8db68b3c00");//c0805
+
             Console.WriteLine("press any key to disappear");
             Console.ReadKey();
         }
